4f9ee7d654536a5e6f7b4ed26e9b80d0f7fab424
Prasad Kanaparthi <prasadmcts@gmail.com>
2019-03-10 15:01:13 +0530

    Added Student & College schemas

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,24 @@
+const db = require('./db')
+
 const Query = {
-    test: () => 'Test Success, GraphQL server is up & running !!'
+    test: () => 'Test Success, GraphQL server is up & running !!',
+    greeting:() => {
+        return "hello, How are you !!!"
+    },
+    sayHello:(root,args,context,info) => `Hi ${args.name} GraphQL server says Hello to you!!`,
+    students:() => db.students.list(),
+    studentById:(root,args,context,info) => {
+        return db.students.get(args.id); //args will contain parameter passed in query
+    },
+}
+
+const Student = {
+    fullName:(root,args,context,info) => {
+       return root.firstName+":"+root.lastName;
+    },
+    college:(root) => {
+       return db.colleges.get(root.collegeId);
+    }
  }
- module.exports = {Query}
+ 
+ module.exports = {Query,Student}
